f33f69d5a6032d90ae450ff0fc949a531f63d3b4
ianfe <ianfe@DESKTOP-GC3QVIU>
2020-04-20 11:44:34 -0700

    Fixed bug where giant jumps above game world

--- core/src/handlers/enemies/GiantHandler.java
+++ core/src/handlers/enemies/GiantHandler.java
@@ -16,6 +16,10 @@ import maps.MapHandler;
  */
 public class GiantHandler {
 
+	private int GIANT_WEWA         = 0;
+	private int GIANT_APALACHICOLA = 1;
+	private int GIANT_PORT_ST_JOE  = 2;
+
 	public static Giant[] giants = new Giant[3];
 
 	private int size = 5;
@@ -25,26 +29,21 @@ public class GiantHandler {
 	 * @param ImageLoader imageLoader
 	 */
 	public void init(ImageLoader imageLoader) {
-		// Wewa giant.
-		giants[0] = new Giant(
+		giants[GIANT_WEWA] = new Giant(
 				GameAttributeHelper.CHUNK_EIGHT_X_POSITION_START + 60, 
 				3, 
 				size, 
 				size, 
 				GameObject.DIRECTION_LEFT
 				);
-
-		// Apalachacola giant.
-		giants[1] = new Giant(
+		giants[GIANT_APALACHICOLA] = new Giant(
 				GameAttributeHelper.CHUNK_EIGHT_X_POSITION_START + 60, 
 				GameAttributeHelper.CHUNK_SIX_Y_POSITION_START + 43, 
 				size, 
 				size, 
 				GameObject.DIRECTION_LEFT
 				);
-
-		// Port St Joe giant.
-		giants[2] = new Giant(
+		giants[GIANT_PORT_ST_JOE] = new Giant(
 				GameAttributeHelper.CHUNK_FOUR_X_POSITION_START + 1, 
 				GameAttributeHelper.CHUNK_THREE_Y_POSITION_START - 6, 
 				size, 
@@ -65,6 +64,13 @@ public class GiantHandler {
 	public void updateGiants(MyGame myGame, MapHandler mapHandler) {
 		for (int i = 0; i < giants.length; i++) {
 			giants[i].updateObject(myGame, mapHandler);
+		}
+		/**
+		 * This giant will eventually be off the map.
+		 * This patch makes that not happen.
+		 */
+		if (giants[GIANT_WEWA].getY() < 0) {
+			giants[GIANT_WEWA].setY(3);
 		}
 	}
 }
