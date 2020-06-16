b7126d2c80d5f78c71101dc1e55657b567601d74
ianfe <ianfe@DESKTOP-GC3QVIU>
2020-06-15 18:44:03 -0700

    Completed cauldron cutscene

--- core/src/handlers/CutSceneHandler.java
+++ core/src/handlers/CutSceneHandler.java
@@ -3,12 +3,14 @@ package handlers;
 import com.mygdx.mygame.MyGame;
 
 import cutscenes.CutSceneBird;
+import cutscenes.CutSceneCauldron;
 import cutscenes.CutSceneCutthroat;
 import cutscenes.CutSceneFarzenplank;
 import cutscenes.CutSceneJollyRoger;
 import gameobjects.weapons.Gun;
 import handlers.enemies.BossHandler;
 import loaders.bossloader.BossLoader;
+import missions.MissionCauldron;
 import missions.MissionRawBar;
 
 /**
@@ -22,12 +24,14 @@ public class CutSceneHandler {
 	private CutSceneCutthroat cutSceneCutthroat;
 	private CutSceneFarzenplank cutSceneFarzenplank;
 	private CutSceneBird cutSceneBird;
+	private CutSceneCauldron cutSceneCauldron;
 
 	public void initializeCutScenes() {
 		cutSceneJollyRoger  = new CutSceneJollyRoger("Cutscene Jolly Roger");
 		cutSceneCutthroat   = new CutSceneCutthroat("Cutscene Cutthroat");
 		cutSceneFarzenplank = new CutSceneFarzenplank("Cutscene Farzenplank");
 		cutSceneBird        = new CutSceneBird("Cutscene Bird");
+		cutSceneCauldron    = new CutSceneCauldron("Cutscene Cauldron");
 	}
 
 	/**
@@ -48,9 +52,11 @@ public class CutSceneHandler {
 		if (MissionRawBar.rawBarMissionComplete) {
 			cutSceneFarzenplank.updateCutScene();
 		}
-
 		if (BossLoader.boss[BossHandler.STUMP_HOLE].isDead()) {
 			cutSceneBird.updateCutScene();
+		}
+		if (MissionCauldron.missionCauldronComplete) {
+			cutSceneCauldron.updateCutScene();
 		}
 	}
 
@@ -68,9 +74,11 @@ public class CutSceneHandler {
 		if (MissionRawBar.rawBarMissionComplete) {
 			cutSceneFarzenplank.renderCutScene(myGame);
 		}
-
 		if (BossLoader.boss[BossHandler.STUMP_HOLE].isDead()) {
 			cutSceneBird.renderCutScene(myGame);
+		}
+		if (MissionCauldron.missionCauldronComplete) {
+			cutSceneCauldron.renderCutScene(myGame);
 		}
 	}
 }
