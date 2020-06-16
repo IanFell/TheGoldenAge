fe9bd1bb9667aed1daf6451be2faab7ecf8375d9
ianfe <ianfe@DESKTOP-GC3QVIU>
2020-06-15 23:19:30 -0700

    Lowered ammo collectible count

--- core/src/handlers/collectibles/AmmoHandler.java
+++ core/src/handlers/collectibles/AmmoHandler.java
@@ -21,7 +21,7 @@ public class AmmoHandler extends CollectibleHandler {
 	public static int ammoCount       = 0;
 	public final static int ammoValue = 5;
 
-	public static final int MAX_AMOUNT_AMMO_ALLOWED_IN_WORLD_TO_COLLECT = 200;
+	public static final int MAX_AMOUNT_AMMO_ALLOWED_IN_WORLD_TO_COLLECT = 75;
 
 	public static final int MAX_AMOUNT_AMMO_PLAYER_CAN_CARRY = 99;
 
